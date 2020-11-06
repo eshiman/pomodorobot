bca7e45da8183c7e7c9a0c8ab3db2b16e6a0cdd2
Esther Shimanovich <esther@localhost.localdomain>
2020-11-06 11:11:17 -0500

    Script cleanup, some docs and some linting

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pomodorobot
-discord pomodoro bot
+This script implements a bot for a discord server, that enables multiple people to coordinate pomodoro breaks together.
+
+In the off-chance that I or anyone else ever recycles this...
+In order to use this script, create a .env file which looks like the following:
+#.env
+DISCORD_TOKEN=insert token
+DISCORD_GUILD="insert name of discord server"
+
+This tutorial has a good explanation on how to set up a bot in Discord's developer portal:
+https://realpython.com/how-to-make-a-discord-bot-python/#how-to-make-a-discord-bot-in-the-developer-portal
--- bot.py
+++ bot.py
@@ -1,64 +1,74 @@
-# bot.py
+"""
+bot.py
+#TODO: remove globals
+"""
+
 import os
 
 import asyncio
 from datetime import datetime
-import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-import time
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
-pom_status = 'OFF'
-pom_init_time = '00:00:00'
+POM_STATUS = 'OFF'
 
 bot = commands.Bot(command_prefix='pomodoro ')
 
-@bot.command(name='on', help='Show pomodoro messages')
-async def pom_on(ctx):
-    global pom_status
-    global pom_init_time
-    pom_status= 'ON'
-    
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    response = "Pomodorobot messages are now visible. (To hide pomodoro messages from this channel, write \"pomodoro off\".)\n\nConcentration time begins at the start of (X:00) and midway through (X:30) each hour. Every 25 minutes, there is a 5 minute break to chat. We are currently " + on_a_break() + ". Happy playing!" 
-    await ctx.send(response)
-    await timer(ctx)
-
-@bot.command(name='off', help='Hide pomodoro messages')
-async def pom_on(ctx):
-    global pom_status
-    pom_status= 'OFF'
-    now = datetime.now()
-    current_time = now.strftime("%H:%M:%S")
-    response = "Pomodorobot messages are now invisible. To turn pomodoro messages on, write \"pomodoro on\"."
-    await ctx.send(response)
-        
-async def timer(ctx):
-    while (pom_status == 'ON'):
-        now = datetime.now()
-        print(now)
-        minutes = now.strftime("%M:%S")
-        current_time = now.strftime("%H:%M:%S")
-        if (minutes == '00:00' or minutes == '30:00'): 
-            response = "Pomodoro session starts now. Play for 25 minutes." 
-            await ctx.send(response)
-        elif (minutes == '25:00' or minutes == '30:00'): 
-            response = "Take a five minute break! Write what you have been doing for the last 25 minutes in the chat."
-            await ctx.send(response)
-        await asyncio.sleep(1)
-
-def on_a_break():
-    now = datetime.now()
-    minutes = now.minute
-    if (minutes >= 25 and minutes < 30):
-        return "on a break"
-    if (minutes > 55 and minutes < 60):
-        return true
+def on_break():
+    """ Returns text indicating current status of pomodoro"""
+    minutes = datetime.now().minute
+    if 25 <= minutes < 30:
+        return "on break"
+    if 55 < minutes < 60:
+        return "on break"
     return "in a pomodoro session"
-        
+
+async def timer(ctx):
+    """Pomodoro timer that lasts as long as POM_STATUS is ON"""
+    global POM_STATUS
+    POM_STATUS= 'ON'
+    print("Timer has activated.")
+    while POM_STATUS == 'ON':
+        minutes = datetime.now().strftime("%M:%S")
+        if (minutes in ['00:00','30:00']):
+            status_message = "~~~\nPomodoro session starts now.\n" \
+"To turn off pomodoro messages, type \"pomodoro off\" into the chat.\n~"
+            await ctx.send(status_message)
+        elif (minutes in ['25:00', '55:00']):
+            status_message = "~~~\nTake a five minute break! " \
+"Write what you have been doing for the last 25 minutes in the chat.\n~"
+            await ctx.send(status_message)
+        await asyncio.sleep(1)
+    # when POM_STATUS is turned off, exit function
+    print("Timer has deactivated.")
+
+@bot.command(name='on', help='Turns on pomodoro timer')
+async def pom_on(ctx):
+    """Starts the pomodoro timer. Time of start does not impact pomodoro's schedule"""
+    # If pomodoro is not already activated, turn it on
+    if POM_STATUS != 'ON':
+        response = "Pomodorobot messages are now visible. " \
+"(To hide pomodoro messages from this channel, write \"pomodoro off\".)\n\n" \
+"Concentration time begins at the start of (X:00) and midway through (X:30) each hour." \
+"Every 25 minutes, there is a 5 minute break to chat. We are currently " \
++ on_break() + ". Happy playing!"
+        await ctx.send(response)
+        await timer(ctx)
+    else:
+        response = "Pomodoro is already on"
+        await ctx.send(response)
+
+
+@bot.command(name='off', help='Turns off pomodoro timer')
+async def pom_off(ctx):
+    """Turns off pomodoro timer"""
+    global POM_STATUS
+    POM_STATUS= 'OFF'
+    response = "Pomodorobot messages are now invisible. " \
+"(To turn pomodoro messages on, write \"pomodoro on\".)"
+    await ctx.send(response)
 
 bot.run(TOKEN)
